ec7677ea5dbc64727b617236689eabb8965418d8
Nathan Petrangelo <nathan@petrangelo.org>
2023-11-18 12:33:41 -0700

    Make 81 element array and transmute it

--- src/main.rs
+++ src/main.rs
@@ -14,9 +14,8 @@ struct Cell {
 }
 
 impl Cell {
-    //  Empty consts used for array initialization
+    //  Empty const used for array initialization
     const NEW: Self = Self::new();
-    const ROW: [Self; 9] = [Self::NEW; 9];
 
     const fn new() -> Self {
         Self { entropy: BTreeSet::new() }
@@ -34,21 +33,22 @@ struct Model {
 }
 
 fn model(_app: &App) -> Model {
-    let cell = Cell::default();
-    let mut row: [Cell; 9] = [Cell::NEW; 9];
-    for i in 0..9 {
-        row[i] = cell.clone();
+    let mut big: [Cell; 81] = [Cell::NEW; 81];
+    for i in 0..81 {
+        big[i] = Cell::default();
     }
-    let mut state: [[Cell; 9]; 9] = [Cell::ROW; 9];
-    for i in 0..9 {
-        state[i] = row.clone();
+    let model: [[Cell; 9]; 9];
+    unsafe {
+        model = std::mem::transmute(big);
     }
     Model {
-        state: [Cell::ROW; 9],
+        state: model,
     }
 }
 
 fn update(_app: &App, _model: &mut Model, _update: Update) {
+    // todo!("Draw sudoku grid");
+    // todo!("Draw stateful sudoku grid");
 }
 
 fn view(app: &App, _model: &Model, frame: Frame) {
