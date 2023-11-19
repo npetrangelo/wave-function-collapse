f2bf1418da3c6da00b8ef498c759788711c7d87e
Nathan Petrangelo <nathan@petrangelo.org>
2023-11-19 01:41:59 -0700

    Trying to keep a reference to a selected cell in Model

--- src/main.rs
+++ src/main.rs
@@ -11,31 +11,48 @@ fn main() {
 }
 
 #[derive(Default)]
-struct Model {
+struct Grid {
     state: [[Cell; 9]; 9],
 }
 
-fn model(app: &App) -> Model {
+struct Model<'a> {
+    grid: Grid,
+    selected: &'a mut Cell,
+}
+
+impl<'a> Model<'a> {
+    fn select(&'a mut self, x: usize, y: usize) {
+        self.selected = &mut self.grid.state[x][y];
+    }
+}
+
+fn model(app: &App) -> Model<'static> {
     let _window = app.new_window()
         .view(view)
         .key_pressed(key_pressed)
         .build()
         .unwrap();
-    Model::default()
+    let mut grid = Grid::default();
+    let mut model = Model {
+        grid,
+        selected: &mut Cell::default(),
+    };
+    model.select(0, 0);
+    model
 }
 
 fn key_pressed(_app: &App, model: &mut Model, key: Key) {
     println!("{:?}", key);
     match key {
-        Key::Key1 => model.state[0][0].collapse(&1),
-        Key::Key2 => model.state[0][0].collapse(&2),
-        Key::Key3 => model.state[0][0].collapse(&3),
-        Key::Key4 => model.state[0][0].collapse(&4),
-        Key::Key5 => model.state[0][0].collapse(&5),
-        Key::Key6 => model.state[0][0].collapse(&6),
-        Key::Key7 => model.state[0][0].collapse(&7),
-        Key::Key8 => model.state[0][0].collapse(&8),
-        Key::Key9 => model.state[0][0].collapse(&9),
+        Key::Key1 => model.selected.collapse(&1),
+        Key::Key2 => model.selected.collapse(&2),
+        Key::Key3 => model.selected.collapse(&3),
+        Key::Key4 => model.selected.collapse(&4),
+        Key::Key5 => model.selected.collapse(&5),
+        Key::Key6 => model.selected.collapse(&6),
+        Key::Key7 => model.selected.collapse(&7),
+        Key::Key8 => model.selected.collapse(&8),
+        Key::Key9 => model.selected.collapse(&9),
         _ => {}
     }
 }
@@ -48,6 +65,6 @@ fn update(_app: &App, _model: &mut Model, _update: Update) {
 fn view(app: &App, model: &Model, frame: Frame) {
     let draw = app.draw();
     draw.background().color(BLACK);
-    model.state[0][0].draw(&draw.x_y(0.0, 0.0));
+    model.grid.state[0][0].draw(&draw.x_y(0.0, 0.0));
     draw.to_frame(app, &frame).unwrap();
 }
